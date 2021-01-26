1b17478eb217fda6bbd9472ffc7852691b8577ec
ivgtr <gear.1112t.o@gmail.com>
2021-01-26 11:52:22 +0900

    :sparkles: add: issueを楽に削除できる様に

---
+++ src/delIssue.ts
@@ -0,0 +1,53 @@
+import dotenv from 'dotenv'
+import axios from 'axios'
+
+dotenv.config()
+
+const url = 'https://api.github.com/graphql'
+
+const headers = {
+  Authorization: `bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+  Accept: 'application/vnd.github.v4.idl'
+}
+
+export default (async () => {
+  try {
+    const issueIds: { id: string }[] = await axios({
+      url,
+      headers,
+      method: 'POST',
+      data: {
+        query: `query {
+          repository(owner:"ivgtr",name:"github-weeklyTrends"){
+            issues(first:100){
+              nodes{
+                id
+              }
+            }
+          }
+        }`
+      }
+    }).then((response) => {
+      return response.data.data.repository.issues.nodes
+    })
+
+    await Promise.all(
+      issueIds.map(async ({ id }) => {
+        await axios({
+          url,
+          headers,
+          method: 'POST',
+          data: {
+            query: `mutation { 
+              deleteIssue(input:{issueId:"${id}"}) { 
+                clientMutationId
+              }
+            }`
+          }
+        })
+      })
+    )
+  } catch (error) {
+    console.log(error)
+  }
+})()
