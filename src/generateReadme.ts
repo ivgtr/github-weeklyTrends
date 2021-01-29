2fd9091cff07b9a67b2d7f5d809fd4dd3f24ad7a
ivgtr <gear.1112t.o@gmail.com>
2021-01-29 14:24:10 +0900

    🚑 fix: バッジを変更

--- src/generateReadme.ts
+++ src/generateReadme.ts
@@ -19,7 +19,7 @@ const createReadme = (newIssueData: { title: string; number: number }) => {
 </div>\n
 ## License\n
 MIT ©[ivgtr](https://github.com/ivgtr)\n
-[![Twitter Follow](https://img.shields.io/twitter/follow/ivgtr?style=social)](https://twitter.com/mawaru_hana) [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE) [![Donate](https://img.shields.io/badge/%EF%BC%84-support-green.svg?style=flat-square)](https://www.buymeacoffee.com/ivgtr)`
+[![Github Follow](https://img.shields.io/github/followers/ivgtr?style=social)](https://github.com/ivgtr) [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE) [![Donate](https://img.shields.io/badge/%EF%BC%84-support-green.svg?style=flat-square)](https://www.buymeacoffee.com/ivgtr)`
 
   fs.writeFileSync('README.md', body)
 }
